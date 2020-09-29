1ff468b5f05d31afb9f9c466f9b14ccecfb78935
MDornacher <manuel.dornacher@gmail.com>
2020-09-29 17:16:24 +0200

    add astropy

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', 'matplotlib==3.3.2', 'scipy==1.5.0', ]
+requirements = ['Click>=7.0', 'astropy==4.0.1.post1', 'matplotlib==3.3.2', 'scipy==1.5.0', ]
 
 setup_requirements = ['pytest-runner', ]
 
