db1a557c35755f082a4a5628c39327878f6513f4
MDornacher <manuel.dornacher@gmail.com>
2020-09-30 12:09:27 +0200

    add parameter summarizer

--- nifty/main.py
+++ nifty/main.py
@@ -4,6 +4,21 @@ from nifty.ui import PlotUI
 from nifty.io import load_spectrum
 
 
+def summarize_input_parameters(args):
+    s = f"""
+    {'-'*40}
+    # NIFTY INPUT PARAMETERS
+    # Input: {args.input}
+    #\tType: {args.type}
+    #\tX-Key: {args.xkey}
+    #\tY-Key: {args.ykey}
+    # Output: {args.output}
+    # Features: {args.features}
+    {'-'*40}
+    """
+    print(s)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input', required=True, help='Specify spectrum input file.')
@@ -14,7 +29,8 @@ if __name__ == '__main__':
     parser.add_argument('-f', '--features', required=True, help='Specify absorption feature input file.')
     args = parser.parse_args()
 
-    if args.a == 'magic.name':
-        print('You nailed it!')
+    summarize_input_parameters(args)
+
+    # TODO: parse / load input parameters
 
     PlotUI()
